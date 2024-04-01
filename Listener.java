d9932f5a080ac61798eaf52590e7d08ba44e3171
thisisvesal <vesal.ea84@gmail.com>
2024-04-01 22:34:59 +0430

    buy/reserve button reactions edited

--- Listener.java
+++ Listener.java
@@ -5,18 +5,48 @@ public class Listener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof Slot_Machine) {
+            Slot_Machine slotMachine = (Slot_Machine)e.getSource();
             System.out.println("slotmachine");
+            if (Utils.board.player1.isOn) {
+                Utils.board.player1.addCoin(new Coin(slotMachine.color));
+            }
 
+            Utils.board.revalidate();
+            Utils.board.repaint();
         } else if (e.getSource() instanceof Card) {
             System.out.println("card");
-            Utils.board.buyButton1.setVisible(true);
+            if (Utils.board.player1.isOn) {
+                Utils.board.buyButton1.setVisible(true);
+                Utils.board.reserveButton1.setVisible(true);
+            } else if (Utils.board.player2.isOn) {
+                Utils.board.buyButton2.setVisible(true);
+                Utils.board.reserveButton2.setVisible(true);
+            }
             // !!!!!!! IMPORTANT STUFF BELOW !!!!!!!
             Utils.board.revalidate();
             Utils.board.repaint();
-        }
-        else if (e.getSource() == Utils.board.buyButton1) {
+        } else if (e.getSource() == Utils.board.buyButton1) {
             System.out.println("buy button 1");
             Utils.board.buyButton1.setVisible(false);
+            Utils.board.reserveButton1.setVisible(false);
+            Utils.board.revalidate();
+            Utils.board.repaint();
+        } else if (e.getSource() == Utils.board.reserveButton1) {
+            System.out.println("reserve button 1");
+            Utils.board.buyButton1.setVisible(false);
+            Utils.board.reserveButton1.setVisible(false);
+            Utils.board.revalidate();
+            Utils.board.repaint();
+        } else if (e.getSource() == Utils.board.buyButton2) {
+            System.out.println("buy button 2");
+            Utils.board.buyButton2.setVisible(false);
+            Utils.board.reserveButton2.setVisible(false);
+            Utils.board.revalidate();
+            Utils.board.repaint();
+        } else if (e.getSource() == Utils.board.reserveButton2) {
+            System.out.println("reserve button 2");
+            Utils.board.reserveButton2.setVisible(false);
+            Utils.board.buyButton2.setVisible(false);
             Utils.board.revalidate();
             Utils.board.repaint();
         }
