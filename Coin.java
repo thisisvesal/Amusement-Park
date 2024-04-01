27f45b37db3703e84d64d3379f7c359fa792fea0
thisisvesal <vesal.ea84@gmail.com>
2024-04-02 02:56:00 +0430

    Transformed class Coin into an extention of JLabel

--- Coin.java
+++ Coin.java
@@ -1,4 +1,7 @@
-public class Coin {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Coin extends JLabel{
     final String color;
     final boolean isSpecial;
     private Player owner;
@@ -7,17 +10,26 @@ public class Coin {
         this.color = color;
         this.isSpecial = isSpecial;
         this.owner = owner;
+        this.setIcon(new ImageIcon("icons/coins/" + color + ".png"));
+        this.setText("" + owner.getCoinCount(color));
+        this.setVerticalTextPosition(JLabel.BOTTOM);
+        this.setHorizontalTextPosition(JLabel.CENTER);
+    }
+
+    public Coin(String color, boolean isSpecial) {
+        this(color, isSpecial, Utils.board.player1);
     }
 
     public Coin(String color, Player owner) {
         this(color, false, owner);
+        this.setText("" + owner.getCoinCount(color));
     }
 
     public Coin(String color) {
         this.color = color;
         this.isSpecial = false;
     }
-    
+
     public void setOwner(Player player) {
         owner = player;
     }
