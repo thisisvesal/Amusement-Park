e34b88a46462644182877839545032f8dc7bd78e
thisisvesal <vesal.ea84@gmail.com>
2024-04-02 04:33:21 +0430

    Added an attribute to determine what card is pressed at the moment

--- Utils.java
+++ Utils.java
@@ -1,4 +1,17 @@
 public class Utils {
     public static final Listener listener = new Listener(); 
     public static final Board board = new Board();
+    private static Card pressedCard;
+
+    public static void arrangeCards() {
+
+    }
+
+    public static void setPressedCard(Card card) {
+        Utils.pressedCard = card;
+    }
+
+    public static Card getPressedCard() {
+        return pressedCard;
+    }
 }
