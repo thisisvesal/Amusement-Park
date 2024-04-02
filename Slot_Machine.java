7be6623b92464aead2a75aec27c145257e50dc5d
thisisvesal <vesal.ea84@gmail.com>
2024-04-03 03:15:38 +0430

    Changes to the way slot machines handle coins + Added the number of coins next to each slot machine

--- Slot_Machine.java
+++ Slot_Machine.java
@@ -1,11 +1,14 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class Slot_Machine extends JButton{
+public class Slot_Machine extends JButton {
     public final String color;
     private int coinCount = 4;
+    private static int pressedMachineCount;
+    private static Slot_Machine[] pressedMachines = new Slot_Machine[3];
 
     public Slot_Machine(String color) {
         this.color = color;
@@ -13,23 +16,63 @@ public class Slot_Machine extends JButton{
         this.setBackground(new Color(48, 181, 199));
         this.setFocusable(false);
         this.addActionListener(Utils.listener);
+        this.setText("" + coinCount);
+        this.setFont(new Font("Tahoma", Font.BOLD, 15));
     }
 
     public int getCoinCount() {
         return coinCount;
     }
 
-    public void giveCoin(Player player, int requestType) {
-        if (requestType == 1) {
-            if (coinCount == 4) {
-                player.addCoin(new Coin(color, player));
-                player.addCoin(new Coin(color, player));
-            } else {
-                requestType = 2; // I don't know if this is okay, change it if needed
-            }
+    public void removeOneCoin() {
+        coinCount--;
+        if (this.coinCount == 0) {
+            this.setEnabled(false);
         }
-        if (requestType == 2) {
-            player.addCoin(new Coin(color, player));
+    }
+
+    public boolean canDoType1() {
+        if (coinCount == 4) {
+            return true;
         }
+        return false;
+    }
+
+    public static boolean isDoneForTheRound() {
+        if (pressedMachineCount == 3) {
+            return true;
+        } else if (pressedMachineCount == 2 && pressedMachines[0] == pressedMachines[1]) {
+            return true;
+        }
+        return false;
+    }
+
+    public void press() {
+        pressedMachines[pressedMachineCount] = this;
+        pressedMachineCount++;
+    }
+
+    public static void reset() {
+        pressedMachineCount = 0;
+        for (int i = 0; i < 3; i++) {
+            pressedMachines[i] = null;
+        }
+
+        if (Utils.board.redMachine.getCoinCount() != 0) {
+            Utils.board.redMachine.setEnabled(true);
+        }
+        if (Utils.board.greenMachine.getCoinCount() != 0) {
+            Utils.board.greenMachine.setEnabled(true);
+        }
+        if (Utils.board.blueMachine.getCoinCount() != 0) {
+            Utils.board.blueMachine.setEnabled(true);
+        }
+        if (Utils.board.whiteMachine.getCoinCount() != 0) {
+            Utils.board.whiteMachine.setEnabled(true);
+        }
+        if (Utils.board.blackMachine.getCoinCount() != 0) {
+            Utils.board.blackMachine.setEnabled(true);
+        }
+
     }
 }
