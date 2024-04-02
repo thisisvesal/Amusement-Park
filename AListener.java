9367a51e24d60fc312cffe01e3117860a5265198
thisisvesal <vesal.ea84@gmail.com>
2024-04-03 03:12:31 +0430

    Renamed the listener class to AListened (actionListener)

--- AListener.java
+++ AListener.java
@@ -1,23 +1,29 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Listener implements ActionListener {
+public class AListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof Slot_Machine) {
             Slot_Machine slotMachine = (Slot_Machine) e.getSource();
             System.out.println("slotmachine");
             if (Utils.board.player1.isOn) {
-                Utils.board.player1.addCoin(new Coin(slotMachine.color));
+                Utils.board.player1.takeCoin(slotMachine);
                 for (int i = 0; i < 6; i++) {
                     if (Utils.board.allCoins1[i].color == slotMachine.color) {
                         Utils.board.allCoins1[i].setText("" + Utils.board.player1.getCoinCount(slotMachine.color));
                     }
                 }
                 System.out.println(Utils.board.player1.getCoinCount(slotMachine.color));
+            } else if (Utils.board.player2.isOn) {
+                Utils.board.player2.takeCoin(slotMachine);;
+                for (int i = 0; i < 6; i++) {
+                    if (Utils.board.allCoins2[i].color == slotMachine.color) {
+                        Utils.board.allCoins2[i].setText("" + Utils.board.player2.getCoinCount(slotMachine.color));
+                    }
+                }
+                System.out.println(Utils.board.player2.getCoinCount(slotMachine.color));
             }
-            Utils.board.revalidate();
-            Utils.board.repaint();
         } else if (e.getSource() instanceof Card) {
             System.out.println("card");
             if (Utils.board.player1.isOn) {
@@ -25,6 +31,7 @@ public class Listener implements ActionListener {
                     Utils.board.buyButton1.setVisible(true);
                     Utils.board.reserveButton1.setVisible(true);
                     Utils.setPressedCard((Card)e.getSource());
+                    System.out.println("Listener reporting, I've set pressed card to " + Utils.getPressedCard());
                 } else {
                     Utils.board.buyButton1.setVisible(false);
                     Utils.board.reserveButton1.setVisible(false);
@@ -49,6 +56,7 @@ public class Listener implements ActionListener {
             System.out.println("buy button 1");
             Utils.board.buyButton1.setVisible(false);
             Utils.board.reserveButton1.setVisible(false);
+            Utils.board.player1.buyCard(Utils.getPressedCard());
             Utils.setPressedCard(null);
             Utils.board.revalidate();
             Utils.board.repaint();
@@ -56,6 +64,7 @@ public class Listener implements ActionListener {
             System.out.println("reserve button 1");
             Utils.board.buyButton1.setVisible(false);
             Utils.board.reserveButton1.setVisible(false);
+            Utils.board.player1.reserve(Utils.getPressedCard());
             Utils.setPressedCard(null);
             Utils.board.revalidate();
             Utils.board.repaint();
@@ -63,6 +72,7 @@ public class Listener implements ActionListener {
             System.out.println("buy button 2");
             Utils.board.buyButton2.setVisible(false);
             Utils.board.reserveButton2.setVisible(false);
+            Utils.board.player2.buyCard(Utils.getPressedCard());
             Utils.setPressedCard(null);
             Utils.board.revalidate();
             Utils.board.repaint();
@@ -73,7 +83,15 @@ public class Listener implements ActionListener {
             Utils.setPressedCard(null);
             Utils.board.revalidate();
             Utils.board.repaint();
+        } else if (e.getSource() == Utils.board.passButton) {
+            System.out.println("Pass button");
+            Utils.switchRound();
         }
 
+        if (Player.getPlayerOfTheRound().isRoundFinished()) {
+            Utils.switchRound();
+        }
+
+        Utils.refreshBoard();
     }
 }
