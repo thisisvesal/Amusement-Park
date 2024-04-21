8f7f5c817944c552ff39567c7e15668fd1dee537
thisisvesal <vesal.ea84@gmail.com>
2024-04-21 23:52:05 +0430

    Added link to github

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+// See this project on GitHub:
+// https://github.com/thisisvesal/Amusement-Park
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Board board = Utils.board;
